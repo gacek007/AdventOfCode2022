fd12d3985548f2f0a4f1505546eae40ba4c6e08b
gacek007 <mateuszborkowski11@gmail.com>
2022-12-03 00:34:45 +0100

    day1 step1

---
+++ day1/1.1/1.1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main()
+{
+    fstream file;
+    string line;
+    int tempcount = 0, tempvalue = 0, max = 0;
+    file.open("input.txt", ios::in);
+    while (getline(file, line)) {
+        if (line != "") {
+            tempvalue = stoi(line);
+            tempcount += tempvalue;
+        }
+        else {
+            if (tempcount > max) {
+                max = tempcount;
+            }
+            tempcount = 0;
+        }
+
+    }
+    cout << max;
+
+
+
+    file.close();
+}
