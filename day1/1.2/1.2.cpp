59536c90d97c642e8dffcda300cc7dc6524fb8ce
gacek007 <mateuszborkowski11@gmail.com>
2022-12-03 00:56:47 +0100

    day1 step2

---
+++ day1/1.2/1.2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main()
+{
+    fstream file;
+    string line;
+    int tempcount = 0, tempvalue = 0, first = 0, second = 0, third = 0, all = 0;
+    file.open("input.txt", ios::in);
+    while (getline(file, line)) {
+        if (line != "") {
+            tempvalue = stoi(line);
+            tempcount += tempvalue;
+        }
+        else {
+            if (tempcount > third) {
+                first = second;
+                second = third;
+                third = tempcount;
+            }
+            all = first + second + third;
+            tempcount = 0;
+        }
+
+    }
+    cout << all;
+
+
+
+    file.close();
+}
